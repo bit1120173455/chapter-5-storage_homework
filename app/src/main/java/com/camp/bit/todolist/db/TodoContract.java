f81d744e229908ef55a7d8dff646801b1d7c5c55
zmy <1120173455@bit.edu.cn>
2019-01-28 23:47:36 +0800

    11

--- app/src/main/java/com/camp/bit/todolist/db/TodoContract.java
+++ app/src/main/java/com/camp/bit/todolist/db/TodoContract.java
@@ -1,5 +1,7 @@
 package com.camp.bit.todolist.db;
 
+import android.provider.BaseColumns;
+
 /**
  * Created on 2019/1/22.
  *
@@ -8,8 +10,20 @@ package com.camp.bit.todolist.db;
 public final class TodoContract {
 
     // TODO 定义表结构和 SQL 语句常量
-
-    private TodoContract() {
+    public static class TodoEntry implements BaseColumns{
+        public static final String TABLE_NAME="tatle";
+        public static final String COLUMNS_DATE="data";
+        public static final String COLUMNS_STATE="state";
+        public static final String COLUMNS_CONTENT="content";
     }
+    public static final String SQL_CREATE_ENTRYS =
+            "CREATE TABLE "+TodoEntry.TABLE_NAME + "(" + TodoEntry._ID
+            +" INTEGER PRIMARY KEY," +TodoEntry.COLUMNS_CONTENT+" TEXT," +TodoEntry.COLUMNS_DATE+" LONG,"
+            +TodoEntry.COLUMNS_STATE+" INTEGER)";
+    public static final String SQL_DELETE_ENTRYS=
+            "DROP TABLE IF EXITS "+ TodoEntry.TABLE_NAME;
+
+    private TodoContract() {}
 
 }
+
